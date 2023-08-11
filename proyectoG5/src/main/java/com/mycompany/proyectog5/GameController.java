34081478856c87eae6894f157047b0e439fef61e
Ariana <arigalda@espol.edu.ec>
2023-08-11 17:50:55 -0500

    borrador del juego

--- proyectoG5/src/main/java/com/mycompany/proyectog5/GameController.java
+++ proyectoG5/src/main/java/com/mycompany/proyectog5/GameController.java
@@ -1,26 +1,143 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.mycompany.proyectog5;
 
+import estructuras.Trie;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Ariana
- */
 public class GameController implements Initializable {
+    private List<String> palabrasJuego;
+    private Trie diccionario;
+    private List<Character> combinacionLista;
+    private int score;
+    private Trie palabras;
+    
+    @FXML
+    private Button buscar;
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Label combinacionLabel;
+
+    @FXML
+    private TextField palabraIngresada;
+
+    @FXML
+    private Label puntaje;
+
+    @FXML
+    private TextArea resultado;
+
+    @FXML
+    private Label tiempo;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        initializeGame();
+    }
     
+    private void initializeGame() {
+        diccionario = new Trie();
+        Diccionario.cargarDiccionario();
+        ///trie con el diccionario
+        diccionario = Diccionario.getDiccionario();
+        //lsita con todas las palabras
+        palabrasJuego = diccionario.getPalabras();
+        System.out.println(palabrasJuego);
+        
+        //trie que usara el juego
+        palabras = new Trie();
+        List<String> palabrasSeleccionadas = new ArrayList<>();
+        Random random = new Random();
+        while(palabrasSeleccionadas.size() < 5){
+            int randomIndex = random.nextInt(palabrasJuego.size());
+            String word = palabrasJuego.get(randomIndex);
+            if(!palabrasSeleccionadas.contains(word)){
+                palabrasSeleccionadas.add(word);
+            }
+        }
+        for(String word : palabrasSeleccionadas){
+            palabras.insert(word, diccionario.getSignificado(word));
+        }
+        
+        //combinacion de letras
+        StringBuilder combinacion = new StringBuilder();
+        for(String word : palabrasSeleccionadas){
+            combinacion.append(word.toUpperCase());
+        }
+        
+        Set<Character> combinacionFinal = new HashSet<>();
+        for(char letra : combinacion.toString().toCharArray()){
+            combinacionFinal.add(letra);
+        }
+        
+        combinacionLista = new ArrayList<>(combinacionFinal);
+        Collections.shuffle(combinacionLista);
+        String combinacionString = combinacionLista.stream().map(String::valueOf).collect(Collectors.joining());
+        combinacionLabel.setText(combinacionString);
+        
+        //temporizador
+        int time = 120; //tiempo en segundos
+        long startTime = System.currentTimeMillis();
+        int score = 0;
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                long currentTime = System.currentTimeMillis();
+                long elapsedTime = currentTime - startTime;
+                long remainingTimeInMillis = TimeUnit.SECONDS.toMillis(time) - elapsedTime;
+
+                if (remainingTimeInMillis <= 0) {
+                    timer.cancel();
+                    Platform.runLater(() -> {
+                    tiempo.setText("Tiempo agotado");
+                    System.out.println("Puntuación final: " + score);
+                    System.out.println("Palabras: ");
+                    System.out.println(palabras.getPalabras());
+                    });
+                } else {
+                    long remainingTimeInSeconds = TimeUnit.MILLISECONDS.toSeconds(remainingTimeInMillis);
+                    Platform.runLater(() -> {
+                        tiempo.setText("Tiempo: " + remainingTimeInSeconds + " segundos");
+                    });
+                }
+            }
+    }, 0, 1000);
+    }
+    
+    @FXML
+    public void buscarPalabra(ActionEvent event){
+        String palabra = palabraIngresada.getText().toUpperCase();
+        System.out.println("Palabra: "+palabra);
+        if (palabra.length() > 0 && combinacionLista.containsAll(palabra.chars().mapToObj(c -> (char) c).collect(Collectors.toList()))) {
+            palabra = Character.toUpperCase(palabra.charAt(0)) + palabra.substring(1).toLowerCase();
+            System.out.println("palabra: "+palabra);
+            if(palabras.search(palabra)){
+                score += palabra.length();
+                puntaje.setText("Puntaje: "+score);
+                resultado.appendText(palabra + ", es una palabra valida!\n");
+            }else{
+                resultado.appendText("No se encontro la palabra "+palabra+"\n");
+            }
+        }else{
+            resultado.appendText("Palabra invalida o letras no disponibles\n");
+        }
+        palabraIngresada.clear();
+    }
 }
--- proyectoG5/src/main/java/estructuras/TrieNode.java
+++ proyectoG5/src/main/java/estructuras/TrieNode.java
@@ -1,6 +1,5 @@
 package estructuras;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
